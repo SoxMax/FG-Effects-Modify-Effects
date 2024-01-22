437f5bbe01b7ef8939be0e2f3e8f4644845bced0
SoxMax <irishsoxmax@gmail.com>
2024-01-21 20:49:00 -0500

    settings for syntax

--- scripts/manager_effect_effect.lua
+++ scripts/manager_effect_effect.lua
@@ -1,12 +1,6 @@
-local addEffect
-local getEffectsBonusByType
-
 function onInit()
     addEffect = EffectManager.addEffect
     EffectManager.addEffect = modifyEffectOnAdd
-
-    getEffectsBonusByType = EffectManager35E.getEffectsBonusByType
-    -- EffectManager35E.getEffectsBonusByType = modifyEffectsBonusByType
 end
 
 function modifyEffectOnAdd(sUser, sIdentity, nodeCT, rNewEffect, bShowMsg, ...)
@@ -29,22 +23,6 @@ function modifyEffectOnAdd(sUser, sIdentity, nodeCT, rNewEffect, bShowMsg, ...)
     end
 
     addEffect(sUser, sIdentity, nodeCT, rNewEffect, bShowMsg, ...)
-end
-
-function modifyEffectsBonusByType(rActor, aEffectType, bAddEmptyBonus, aFilter, rFilterActor, bTargetedOnly, ...)
-    local effects, effectsCount = getEffectsBonusByType(rActor, aEffectType, bAddEmptyBonus, aFilter, rFilterActor, bTargetedOnly, ...)
-    if effectsCount > 0 then
-        bonusMods = getBonusMods(rActor)
-        if next(bonusMods) then
-            for bonusType, effect in pairs(effects) do
-                local bonusMod = bonusMods[bonusType]
-                if bonusMod then
-                    effect.mod = effect.mod + bonusMod
-                end
-            end
-        end
-    end
-    return effects, effectsCount
 end
 
 function getBonusMods(rActor)
