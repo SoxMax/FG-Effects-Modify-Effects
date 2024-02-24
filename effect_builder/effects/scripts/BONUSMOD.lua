447bbfb145255069960a158b1813174c13fc9637
SoxMax <irishsoxmax@gmail.com>
2024-02-24 13:30:17 -0500

    update with new criteria (#3)

--- effect_builder/effects/scripts/BONUSMOD.lua
+++ effect_builder/effects/scripts/BONUSMOD.lua
@@ -1,8 +1,16 @@
 function createEffectString()
     local effectString = parentcontrol.window.effect.getStringValue() .. ": " .. effect_modifier.getValue()
+    local descriptors = {}
     if not effect_bonus_type.isEmpty() then
-        effectString = effectString .. " " .. effect_bonus_type.getValue()
+        table.insert(descriptors, effect_bonus_type.getValue())
+    end
+    local effectGeneration = effect_generation.getStringValue()
+    if effectGeneration ~= "" then
+        table.insert(descriptors, effectGeneration)
     end
 
+    if #descriptors > 0 then
+        effectString = effectString .. " " .. table.concat(descriptors, ",")
+    end
     return effectString
 end
